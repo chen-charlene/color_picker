3e11ddae566d33ac409f73c842185eac9e81eb56
chen-charlene <113152162+chen-charlene@users.noreply.github.com>
2024-12-14 21:06:05 -0500

    Upload files to git

---
+++ wdt.ino
@@ -0,0 +1,30 @@
+/* Get next unused CPU interrupt > start */
+unsigned int getNextCPUINT(unsigned int start) {
+   unsigned int tryInt = start + 1;
+      while (tryInt < 32) {
+         if (NVIC_GetEnableIRQ((IRQn_Type) tryInt) == 0) {
+            return tryInt;
+         }
+      tryInt++;
+   }
+}
+
+// method to iniialize watchdog to reset system if not pet within timeframe
+void initWDT () {
+  // cks
+  R_WDT->WDTCR = (0b1000 << R_WDT_WDTCR_CKS_Pos) |
+    (0b11 << R_WDT_WDTCR_RPES_Pos) | (0b11 << R_WDT_WDTCR_RPSS_Pos) | (0b10 << R_WDT_WDTCR_TOPS_Pos);
+
+  // watchdog trigger interrupt
+  R_WDT->WDTRCR = (0b1 << 7);
+  R_ICU->IELSR[WDT_INT] = (0x025 << R_ICU_IELSR_IELS_Pos);
+}
+
+// method to pet watchdog to reset timer
+void petWDT() {
+  // Serial.println("PET DOG");
+  R_WDT-> WDTRR = 0x00;
+  R_WDT-> WDTRR = 0xff;
+}
+
+
