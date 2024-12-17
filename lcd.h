e79473abe265be9cb8faf44187eeb93267ed020d
camsik <113078605+camsik@users.noreply.github.com>
2024-12-17 10:37:48 -0500

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,8 @@
+#define LCD_WIDTH   240 //LCD width
+#define LCD_HEIGHT  320 //LCD height
+
+// screen initialization method
+void LCD_Init();
+
+// method to update the screen with color
+void LCD_Clear(uint16_t color);
